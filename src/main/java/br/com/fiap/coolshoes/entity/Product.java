930fb7553c10188762b9dc1e9ece484930e29167
sheldongomes <68485686+sheldongomes@users.noreply.github.com>
2020-09-16 22:39:57 -0300

    'Teste'

---
+++ src/main/java/br/com/fiap/coolshoes/entity/Product.java
@@ -0,0 +1,12 @@
+package br.com.fiap.coolshoes.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_PRODUCT")
+public class Product {
+
+}
